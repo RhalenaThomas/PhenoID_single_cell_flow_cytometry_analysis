659564e3c37304a2afc2a297fa17070b27ea07d8
Rhalena Thomas <rhalena.t@gmail.com>
2022-11-01 17:09:12 -0400

    phenoid library

--- PhenoID_library/Preprocessing.R
+++ PhenoID_library/Preprocessing.R
@@ -1,8 +1,27 @@
 # Data preprocessing functions
+# fsc_to_df                        (Rhalena)
+# harmonize                        (Rhalena)
+# df_to_seurat                     (Rhalena)
 
+
+##############################################################################################
 # fsc_to_df
-# readis in fsc files in a folder to creat and R list and save a dataframe
+# reads in fsc files in a folder to creat and R list and save a dataframe
+# sample names are input by user
 
+
+
+
+
+
+
+##############################################################################################
 # harmonize
 # transform, align, retro transform flowset object and save a csv file
 
+
+
+
+##############################################################################################
+# df_to_seurat
+# creates a seurat object from the expression matrix and adds in meta data
